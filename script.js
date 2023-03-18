ea4e11b0692b75d684fc7cfd5784514236cc1aa3
hussain1121-er <65068496+hussain1121-er@users.noreply.github.com>
2023-03-18 17:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+let dd = document.getElementById('dd');
+let hh = document.getElementById('hh');
+let mm = document.getElementById('mm');
+let ss = document.getElementById('ss');
+
+let countdown = document.getElementById("countdown");
+let wishDays = 10;
+var itsWeddingDay = false;
+let x = setInterval(function () {
+  let currentYear = new Date().getFullYear();
+  let newYear = new Date(`Sept 8, 2023 00:00:00`);
+  let now = new Date().getTime();
+  let distance = newYear - now;
+
+  // time calculation
+  let d = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let s = Math.floor((distance % (1000 * 60)) / (1000));
+
+  // output the result
+  days.innerHTML = d + "<br><span>Days</span>";
+  hours.innerHTML = h + "<br><span>Hours</span>";
+  minutes.innerHTML = m + "<br><span>Minutes</span>";
+  seconds.innerHTML = s + "<br><span>Seconds</span>";
+
+  // animate stroke
+  dd.style.strokeDashoffset = 440 - (440 * d) / 365;
+  hh.style.strokeDashoffset = 440 - (440 * h) / 24;
+  mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+  ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+  // if countdown is over, change the innerText of .text
+  if (distance < 0) {
+    document.querySelector('#message').innerText = `Happily Married `;
+    countdown.style.display = "none";
+  }else if(distance == 0){
+    document.querySelector('#message').innerText = `Its Wedding Day`;
+    itsWeddingDay = true;
+    countdown.style.display = "";
+  }else {
+    countdown.style.display = "";
+  }
+}, 500);
+
+
+
+
+const body = document.querySelector("body");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "fas fa-heart";
+    heart.style.left = (Math.random() * 100)+"vw";
+    heart.style.animationDuration = (Math.floor(Math.random() * 10) + 1)+"s"
+    body.appendChild(heart);
+}
+setInterval(createHeart,100);
+setInterval(function name(params) {
+    var heartArr = document.querySelectorAll(".fa-heart")
+    if (heartArr.length > 200) {
+       heartArr[0].remove()
+    }
+    //console.log(heartArr);
+},100)
